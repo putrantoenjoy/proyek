d4f96f6a83ff0f58d35f4ba93afd2dbbabd44e69
Almaaslutfi <almaslutfi17@gmail.com>
2024-02-05 14:50:18 +0700

    update web auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\formController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
